75cd7795a3ae4468a8ba9309f8f84be0fe280ec2
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-19 07:50:26 +0000

    [add&機能追加]新規登録ログインログアウトができるようにした。

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2024_08_19_045139) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "名前"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
