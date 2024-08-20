7bfc01dc2ae3ef746687742204f9ed8aed34c682
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-20 02:24:49 +0000

    [add]booksコントローラーを作成し、投稿ページを作った。

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+  def new
+    @book=Book.new
+  end
+
+  def create
+    book=Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path
+  end
+
+  def index
+  end
+  
+  def edit
+  end
+
+  def show
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:bo)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root 'homes#top'
   devise_for :users
+  get 'homes/about' => 'homes#about' ,as: 'about'
+  resources:books,only:[:new,:create,:index,:edit,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'homes/about' => 'homes#about' ,as: 'about' 
 end
