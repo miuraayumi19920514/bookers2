1adf52be16a5d7ae73a0ab21da36db6fe906998c
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-21 07:49:36 +0000

    [add]必要なページ４つのうち3つに共通表示をさせた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,25 +4,29 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path
+    @book=Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
+    @book=Book.new
+    @user=current_user
+    @books=Book.all
   end
-  
+
   def edit
   end
 
   def show
+    @book=Book.find(params[:id])
   end
-  
+
   private
-  
+
   def book_params
-    params.require(:book).permit(:title,:bo)
+    params.require(:book).permit(:title,:body)
   end
-  
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @book=Book.new
+    @user=current_user
+  end
+
+  def show
+    @book=Book.find(params[:id])
+    @user=@book.user
+  end
+
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    user=User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image , :introduction)
+  end
+
+end
